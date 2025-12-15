c5a5bfd51056779550556d8153cea0d560ec2ab0
gringografov-boop <gringo.grafov@gmail.com>
2025-12-15 20:16:08 +0300

    fix tests: ml config, uploads

--- app/main.py
+++ app/main.py
@@ -3,7 +3,8 @@ from app.ml.router import router as ml_router
 from app.core.config import PROJECT_NAME, SERVER_PORT
 from app.core.database import setup_db
 from app.utils.server_utils import setup
-
+import os
+os.makedirs("uploads", exist_ok=True)
 app = FastAPI(title=f"{PROJECT_NAME}")
 app.include_router(ml_router, prefix="/api/v1")
 setup(app)
--- app/ml/config.py
+++ app/ml/config.py
@@ -4,7 +4,7 @@ import os
 
 class MLConfig(BaseSettings):
     # API ключ для OpenRouter
-    OPENROUTER_API_KEY: str = os.getenv("OPENROUTER_API_KEY")
+    OPENROUTER_API_KEY: str = os.getenv("OPENROUTER_API_KEY", "")
     OPENROUTER_BASE_URL: str = "https://openrouter.ai/api/v1"
     MODEL_NAME: str = "openai/gpt-5-mini"
     
